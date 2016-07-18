83cd839c8ba0a55387b33b049c5c99d0fbad1dcf
matteobarone <matteobarone90@alice.it>
2016-07-18 22:51:57 +0200

    updating portfolio v2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-//// Script to open and close the PLUS section - No JQuery ////
+// Script to open and close the PLUS section - No JQuery ////
 
 // Store objects in variables
 var piu = document.getElementById("piu");
@@ -6,7 +6,12 @@ var close = document.getElementById("closeX");
 var circle = document.getElementById("circle");
 var bio = document.getElementById("bio");
 var versionBox = document.getElementById("versionBox");
+
+var wrapperProjectsAbout = document.getElementById("wrapper-projects-about");
+
 var projects = document.getElementById("projects");
+var about = document.getElementById("about");
+var aboutTitle = document.getElementById("about-title");
 var body = document.getElementsByTagName("BODY")[0];
 
 // Add the event listener
@@ -31,7 +36,7 @@ function openPiu(){
 function closePiu(){
     piu.classList.remove("expandPlus");
     close.classList.toggle("animate-x");
-    projects.classList.toggle("showProjects");
+    wrapperProjectsAbout.classList.remove("show");
     body.classList.toggle("noBorderBody");
     body.classList.toggle("heightAuto");
     
@@ -50,7 +55,7 @@ function goToHomePage(){
 
 
 
-//Video Controller
+//Video Controller play/pause in the single project page
 
 var mobileAtmVideo = document.getElementById("mobile-atm-video");
 var mobileControllerPlay = document.getElementById("mobile-controller-play");
@@ -78,7 +83,7 @@ function pauseVid() {
 
 
 
-//Controller for the hover in the about: active/disactive song
+//Controller for the hover in the about: active/disactive song - JQUERY
 
 var figure = $(".about-img").hover( hoverVideo, hideVideo );
 
@@ -98,47 +103,67 @@ function hideVideo(e) {
 
 
 
+// Only for about page, when the brackets in the corner of the page reach the About section (blue section), its become white
+if(about){
 
+    window.onscroll = function(e) {
 
+        //about offset
+        var offsetAbout = about.getBoundingClientRect().top;
 
+        //get brackets
+        var bracketTopLeft = document.getElementById("bracket-top-left");
+        var bracketTopRight = document.getElementById("bracket-top-right");
+        var bracketBottomLeft = document.getElementById("bracket-bottom-left");
+        var bracketBottomRight = document.getElementById("bracket-bottom-right");
+        //get x
+        var x = document.getElementById("closeX");
 
-window.onscroll = function(e) {
-    
-    //get brackets
-    var bracketTopLeft = document.getElementById("bracket-top-left");
-    var bracketTopRight = document.getElementById("bracket-top-right");
-    var bracketBottomLeft = document.getElementById("bracket-bottom-left");
-    var bracketBottomRight = document.getElementById("bracket-bottom-right");
-    
-    // Get the pageYOffset
-    var pageOffset = window.pageYOffset;
-    
-    //browser height
-    var browserheight = window.innerHeight;
-    
-    // Page height minus the browser height
-    var totalHeightMinusBrowser = document.body.clientHeight - window.innerHeight;
-    
-    //about offset
-    var about = document.getElementById("about");
-    var offsetAbout = about.getBoundingClientRect().top;
-    
-    var offsetBottomBrackets = offsetAbout - browserheight + 55;
-    
-    
-    console.log("browserHeight -> " + browserheight);
-    console.log("bottom brackets offset -> " + offsetBottomBrackets);
-    
-    if(offsetBottomBrackets < 0){
-        bracketBottomLeft.classList.add("white-brackets-bottom-left");
-        bracketBottomRight.classList.add("white-brackets-bottom-right");
-    }else{
-        bracketBottomLeft.classList.remove("white-brackets-bottom-left");
-        bracketBottomRight.classList.remove("white-brackets-bottom-right");
+        // Get the pageYOffset
+        var pageOffset = window.pageYOffset;
+
+        //browser height
+        var browserheight = window.innerHeight;
+
+        // Page height minus the browser height
+        var totalHeightMinusBrowser = document.body.clientHeight - window.innerHeight;
+
+        //calculation of the offset for brackets and x
+        var offsetBottomBrackets = offsetAbout - browserheight + 35;
+        var offsetTopBrackets = offsetAbout - 20;
+        var offsetX = offsetAbout - 50;
+
+        //console.log("browserHeight -> " + browserheight);
+        //console.log("top brackets offset -> " + offsetTopBrackets);
+        //console.log("offsetX -> " + offsetX);
+
+        if(offsetBottomBrackets < 0){
+            bracketBottomLeft.classList.add("white-brackets");
+            bracketBottomRight.classList.add("white-brackets");
+        }else{
+            bracketBottomLeft.classList.remove("white-brackets");
+            bracketBottomRight.classList.remove("white-brackets");
+        }
+        
+        if(offsetTopBrackets < 0){
+            bracketTopLeft.classList.add("white-brackets");
+            bracketTopRight.classList.add("white-brackets");
+        }else{
+            bracketTopLeft.classList.remove("white-brackets");
+            bracketTopRight.classList.remove("white-brackets");
+        }
+        
+        if(offsetX < 0){
+            x.classList.add("x-white");
+        }else{
+            x.classList.remove("x-white");
+        }
+        
     }
-
-
+    
 }
+
+
 
 
 
