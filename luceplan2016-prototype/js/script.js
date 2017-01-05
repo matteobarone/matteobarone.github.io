9b431f7dc66a940581a5d811c4f9d94af750306b
Matteo Barone <matteobarone90@alice.it>
2017-01-05 11:17:32 +0100

    luceplan test

---
+++ luceplan2016-prototype/js/script.js
@@ -0,0 +1,33 @@
+// Script for Search Panel
+
+if( document.querySelector('.search-container') != null){
+
+	function openCloseSearchPanel(){
+		document.body.classList.toggle('disable-body');
+		document.querySelector('.opacize-body').classList.toggle('on');
+		document.querySelector('.search-container').classList.toggle('on');
+	}
+
+	function openCloseMobileNav(){
+		document.body.classList.toggle('disable-body');
+		document.querySelector('.opacize-body').classList.toggle('on');
+		document.querySelector('.nav-mobile').classList.toggle('on');
+	}
+
+	document.querySelector('#searchIcon').addEventListener('click', openCloseSearchPanel);
+	document.querySelector('#closeSearch').addEventListener('click', openCloseSearchPanel);
+
+	document.querySelector('#hamburger').addEventListener('click', openCloseMobileNav);
+	document.querySelector('#closeMobileNav').addEventListener('click', openCloseMobileNav);
+
+	document.querySelector('#filtersArrow').addEventListener('click', function(){
+		document.querySelector('.filters-box').classList.toggle('show');
+		document.querySelector('#filtersArrow img').classList.toggle('rotate-arrow');
+	});
+
+	if(window.innerWidth < 480){
+		document.querySelector('#searchBar').setAttribute('placeholder', 'Search');
+	}
+
+}
+
