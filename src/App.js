56b2da56b30e238db2897a23eb26a491430cb857
matteobarone <barmat90@gmail.com>
2018-07-16 23:11:56 +0200

    feat(app): styled-components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #FAFAFA;
+`;
+
+const Logo = styled.h1`
+  color: #0028FF;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Wrapper>
+        <Logo>MB</Logo>
+      </Wrapper>
     );
   }
 }
