0be53305df95f14e0e79f0158421b71b36fd6872
matteobarone <barmat90@gmail.com>
2019-01-31 01:08:45 +0100

    feat(heart): experiment with anime

---
+++ heart/precache-manifest.850142a59e0b6c2521c0c9d416196b4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d015cc439608e3fa878b",
+    "url": "/heart/static/js/runtime~main.d015cc43.js"
+  },
+  {
+    "revision": "5e5ea43ed000de31d26a",
+    "url": "/heart/static/js/main.5e5ea43e.chunk.js"
+  },
+  {
+    "revision": "f142bd701604a29707bc",
+    "url": "/heart/static/js/1.f142bd70.chunk.js"
+  },
+  {
+    "revision": "5e5ea43ed000de31d26a",
+    "url": "/heart/static/css/main.3e3d5dd5.chunk.css"
+  },
+  {
+    "revision": "e8ab228f004ef8b271883ac9afdf4aab",
+    "url": "/heart/index.html"
+  }
+];
