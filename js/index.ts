c7382c331b27dce04e1e4048c3e4f4aa313693af
Matteo Barone <barmat90@gmail.com>
2019-12-13 16:31:55 +0100

    feat(app): init restyle

--- js/index.ts
+++ js/index.ts
@@ -1,14 +1,1 @@
 import '../css/index.css';
-import {pointer} from './animations/pointer';
-import {changeColor} from './animations/changeColor';
-
-let randomNumber = Math.floor(Math.random() * 5) + 1;
-const BACKGROUND_HEADER_INTERVAL = 5000;
-
-window.addEventListener('load', init, false);
-
-function init() {
-  randomNumber = changeColor(randomNumber);
-  setInterval(() => randomNumber = changeColor(randomNumber), BACKGROUND_HEADER_INTERVAL);
-  pointer();
-}
