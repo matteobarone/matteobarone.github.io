5acc97f87a268f5f0d6b5ce4da64e3c869ca458e
Matteo Barone <barmat90@gmail.com>
2019-12-18 18:25:06 +0100

    feat(home): small animations

--- js/index.ts
+++ js/index.ts
@@ -1,6 +1,28 @@
 import '../css/index.css';
 import {imageLoader} from './image-loader';
 
+let windowScrollY = window.scrollY;
+
 document.addEventListener('DOMContentLoaded', () => {
   imageLoader();
+
+  const headerElement = document.querySelector('header');
+  headerElement.classList.add('show--translate');
+
+  window.addEventListener('scroll', () => {
+    window.scrollY > windowScrollY
+      ? hasHeaderShowClass() && headerElement.classList.remove('show--translate')
+      : !hasHeaderShowClass() && headerElement.classList.add('show--translate');
+
+    windowScrollY = window.scrollY;
+  });
+
+  function hasHeaderShowClass() {
+    return headerElement.classList.contains('show--translate');
+  }
+
+  const selectorsToAnimate = ['main', 'section'];
+  selectorsToAnimate.forEach(selector => {
+    document.querySelector(selector).classList.add('show--opacity');
+  })
 });
